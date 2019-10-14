4f3ee4da03ac90ef211ca584f7f701e7543955ba
DevFern <45023645+Suchithfernando@users.noreply.github.com>
2019-10-14 11:11:00 +0530

    Add files via upload

---
+++ avg by decimals/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float no1,no2,avg;
+    printf("Enter a number ");
+    scanf("%f",&no1);
+    printf("Enter a number");
+    scanf("%f",&no2);
+    avg=(no1+no2)/2;
+    printf("The average is %3f\n",avg);
+    return 0;
+}
